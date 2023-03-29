a0d8019d97833c15a425b758402f75d3ed5880d0
Julie Krav <j.krav07@gmail.com>
2023-03-29 12:26:25 -0700

    queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,27 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  let tailPosition = 0;
+  let headPosition = 0;
 
   someInstance.enqueue = function(value) {
+    storage[tailPosition] = value;
+    tailPosition++;
   };
 
   someInstance.dequeue = function() {
+    let firstString = storage[headPosition];
+    delete storage[headPosition];
+    headPosition++;
+    return firstString;
   };
 
   someInstance.size = function() {
+    let sizeCounter = 0;
+    for (let prop in storage) {
+      sizeCounter++;
+    }
+    return sizeCounter;
   };
 
   return someInstance;
