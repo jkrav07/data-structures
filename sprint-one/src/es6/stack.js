5ff7d5700b6091de368d2b3cb6a90f06eb077932
Julie Krav <j.krav07@gmail.com>
2023-03-29 16:31:58 -0700

    ES6: stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.top = -1;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.top++;
+    this.storage[this.top] = value;
+  }
+
+  pop() {
+    if (this.top > -1) {
+      let topString = this.storage[this.top];
+      delete this.storage[this.top];
+      this.top--;
+      return topString;
+    }
+  }
+
+  size() {
+    return this.top + 1;
   }
 
 }
