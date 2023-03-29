64dbd62092b511e60814806c802e75218eaa3f87
Julie Krav <j.krav07@gmail.com>
2023-03-29 16:03:26 -0700

    Pseudoclassical: stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = -1;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.top++;
+  debugger;
+  this.storage[this.top] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.top > -1) {
+    let topString = this.storage[this.top];
+    delete this.storage[this.top];
+    this.top--;
+    return topString;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.top + 1;
+};
 
